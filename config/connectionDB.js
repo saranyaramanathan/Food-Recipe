27125fb09d1108115087a5ba215f953258fb4f05
Saranya <saranya@credentech.net>
2025-07-24 18:46:57 +0530

    error fix

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -2,5 +2,6 @@ const mongoose=require("mongoose");
 const connectDB = async()=>{
     await mongoose.connect(process.env.CONNECTION_URL)
     .then(()=>{console.log("DB connected")})
+    .catch(err => console.error("Mongo connection error:", err));
 }
 module.exports=connectDB
